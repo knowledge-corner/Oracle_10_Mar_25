872691220c74273fbb1694c70a86e0204ceaf680
knowledge-corner <vaidehi.nair@knowledgecorner.in>
2025-03-12 15:04:12 +0530

    update

--- Classwork/demo.py
+++ Classwork/demo.py
@@ -1,11 +1,4 @@
 import util
 
-print("Main Program started")
 print("Main Program calling factorial - ", util.factorial(5))
-# print(util.is_even("abcd"))
-
-# @util.is_int
-# def square(num):
-#     return num**2
-
-# print(square("abcd"))
+print(util.add(2, 3))
--- Classwork/util.py
+++ Classwork/util.py
@@ -1,16 +1,11 @@
 # Assumption - This is a util library and contains functions for numeric calculations
 
-print("Start of util module")
-
 def is_int(func):  # func is the function object and the function with the decorater is passed to func variable
-    print("Inside is_int")
-    def check(param) : # param takes the parameters passed to the calling function
-        print("Inside check")
-        if type(param) == int :
-            print("Inside check - if condition is true")
-            return func(param)
+    def check(*param) : # param takes the parameters passed to the calling function      
+        # Check if all the values in the param tuple are int or not       
+        if all(map(lambda num : type(num) == int, param)) :           
+            return func(*param)
         else:
-            print("Inside check - if condition is false")
             return "Error in the parameter"
     return check
 
@@ -21,9 +16,11 @@ def factorial(num) :
         fact *= i
     return fact
 
+@is_int
 def is_even(num) :
     return num % 2 == 0
 
+@is_int
 def is_prime(num):
     for i in range(2, num) :
         if num % i == 0 :
@@ -31,5 +28,7 @@ def is_prime(num):
     else:
         return True
 
+@is_int
 def add(num1, num2) :
     return num1 + num2
+
